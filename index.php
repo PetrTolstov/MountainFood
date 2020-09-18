4d132587da328e439220728a8cccbdcb5bd3dae3
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2020-09-18 20:20:46 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+$db = new PDO('mysql:host=localhost; dbname=mountainfood','mountainfood', 'zxcvbnm');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name = "description" content = "Fastfood-cafe" > <!-- мета элемент, который описывает содержимое документа -->
+    <meta name = "keywords" content = "Burger, CocaCola"> <!-- мета элемент, который описывает ключевые слова в документе -->
+    <meta name = "author" content = "Franz Fertich"> <!-- мета элемент, который указывает имя автора документа -->
+    <meta charset="UTF-8" http-equiv="refresh" content="9999" >
+    <!--<meta  http-equiv  = "content-security-policy" content = "default-src https:" > мета элемент, который разрешает загрузку ресурсов (изображения, шрифты, скрипты и так далее) только с использованием https -->
+    <meta name = "robots" content = "index, follow"> <!-- Используется поисковыми системами при индексации страниц и прохода по ссылкам поисковой системой-->
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1"> <!--Для того, чтобы мобильные браузеры автоматически не изменяли размер страниц сайта-->
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <title>Lesson 1</title>
+    <link rel="icon" href="style/icon.png" sizes="16x16 32х32" type="image/png">
+    <link href="style/page_styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">Our Food</a></li>
+            <li><a href="">Last news</a></li>
+            <li><a href="">Favorites Meals</a></li>
+            <li><a href="">About us</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+
+<div id='main'>
+<pre>dfsfsdfs
+hfd
+h
+fdh
+fdghf
+h
+fdh
+
+dghf
+gh
+dfgh
+dfg
+h
+fdh</pre>
+</div>
+
+
+
+</body>
+
+</html>
+
+
+
